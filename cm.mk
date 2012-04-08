cc805935339b8fb1dc8287120b2fde3e80d9c1e7
mantera <som4th@gmail.com>
2012-04-08 01:04:08 -0400

    Update build id

--- cm.mk
+++ cm.mk
@@ -35,11 +35,11 @@ UTC_DATE := $(shell date +%s)
 DATE     := $(shell date +%Y%m%d)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_ID=IML74K \
+    BUILD_ID=IML77 \
     PRODUCT_NAME=triumph \
     TARGET_DEVICE=triumph \
-    BUILD_FINGERPRINT=motorola/motorola_triumph/triumph:4.0.3/IML74K/223971:user/release-keys \
-    PRIVATE_BUILD_DESC="triumph-user 4.0.3 IML74K 223971 release-keys" \
+    BUILD_FINGERPRINT=motorola/motorola_triumph/triumph:4.0.4/IML77/223971:user/release-keys \
+    PRIVATE_BUILD_DESC="triumph-user 4.0.4 IML77 223971 release-keys" \
     BUILD_NUMBER=${DATE} \
     BUILD_VERSION_TAGS=release-keys \
     TARGET_BUILD_TYPE=user
