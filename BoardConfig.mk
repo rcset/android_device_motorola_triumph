50a7ab87e0edae7ca9c85cfc906ddb615de5366c
mantera <som4th@gmail.com>
2012-07-13 19:44:45 -0400

    BoardConfig: add defines for triumph

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -49,8 +49,9 @@ TARGET_PROVIDES_RELEASETOOLS := true
 TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := ./device/motorola/triumph/releasetools/triumph_img_from_target_files
 TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := ./device/motorola/triumph/releasetools/triumph_ota_from_target_files
 
+TARGET_TRIUMPH := true
 BOARD_USES_QCOM_HARDWARE := true
-COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE
+COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE -DTRIUMPH
 BOARD_USES_QCOM_LIBS := true
 BOARD_USES_QCOM_LIBRPC := true
 BOARD_USE_QCOM_PMEM := true
