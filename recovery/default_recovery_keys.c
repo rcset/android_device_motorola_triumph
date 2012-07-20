d846c78679186e7064ff1c35147e5055ec7534a8
mantera <som4th@gmail.com>
2012-07-20 09:28:42 -0400

    Recovery: update default_recovery_keys to current

--- recovery/default_recovery_keys.c
+++ recovery/default_recovery_keys.c
@@ -4,6 +4,7 @@
 #include "common.h"
 #include "extendedcommands.h"
 
+
 int device_toggle_display(volatile char* key_pressed, int key_code) {
     int alt = key_pressed[KEY_LEFTALT] || key_pressed[KEY_RIGHTALT];
     if (alt && key_code == KEY_L)
@@ -13,7 +14,7 @@ int device_toggle_display(volatile char* key_pressed, int key_code) {
         return 0;
         //return get_allow_toggle_display() && (key_code == KEY_HOME || key_code == KEY_MENU || key_code == KEY_END);
     }
-    return get_allow_toggle_display() && (key_code == KEY_POWER || key_code == KEY_END);
+    return get_allow_toggle_display() && (key_code == KEY_HOME || key_code == KEY_MENU || key_code == KEY_POWER || key_code == KEY_END);
 }
 
 int device_handle_key(int key_code, int visible) {
@@ -29,7 +30,7 @@ int device_handle_key(int key_code, int visible) {
                 return SELECT_ITEM;
 
             case KEY_BACK:
-                if (ui_menu_level > 0) {
+                if (!ui_root_menu) {
                     return GO_BACK;
                 }
         }
