753db61cc060683c78b5462602743ff05d16667c
Gannon Macdonald <gannon050197@gmail.com>
2012-09-07 18:44:59 -0500

    Adding in Lloir's vendor/qcom/proprietary

--- device_triumph.mk
+++ device_triumph.mk
@@ -250,6 +250,9 @@ PRODUCT_AAPT_PREF_CONFIG := hdpi
 # we have enough storage space to hold precise GC data
 PRODUCT_TAGS += dalvik.gc.type-precise
 
+# inherit vendor/qcom/proprietary
+$(call inherit-product, vendor/qcom/proprietary/qcom-vendor.mk)
+
 $(call inherit-product, build/target/product/full_base.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
